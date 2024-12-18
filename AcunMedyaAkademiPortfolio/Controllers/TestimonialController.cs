0e97a3e477a2cc625943d7bc086bbfb61cc8bd32
Atamany <yigitataman1@gmail.com>
2024-12-19 00:42:05 +0300

    7.

--- AcunMedyaAkademiPortfolio/Controllers/TestimonialController.cs
+++ AcunMedyaAkademiPortfolio/Controllers/TestimonialController.cs
@@ -15,5 +15,30 @@ namespace AcunMedyaAkademiPortfolio.Controllers
             var values = db.TblTestimonial.ToList();
             return View(values);
         }
+        [HttpGet]
+        public ActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateTestimonial(TblTestimonial p)
+        {
+            db.TblTestimonial.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var value = db.TblTestimonial.Find(id);
+            db.TblTestimonial.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var value = db.TblTestimonial.Find(id);
+            return View(value);
+        }
     }
 }
